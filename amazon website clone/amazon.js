183f8e016262535c796a7ebd591d31020160fa5a
Mariama Seidu <153280802+Marie-297@users.noreply.github.com>
2024-04-10 20:21:44 +0100

    moved the addtocart function into the cart folder and exported it to amazon.js

--- amazon website clone/amazon.js
+++ amazon website clone/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from "./data/cart.js";
+import {cart, addToCart} from "./data/cart.js";
 import {products} from "./data/products.js";
 
 let productsHTML = "";
@@ -54,23 +54,6 @@ products.forEach((product) => {
       </button>
     </div>`
 })
-
-function addToCart(productId) {
-  let matchingItem = "";
-    cart.forEach((cartItem) => {
-      if (productId === cartItem.productId) {
-        matchingItem = cartItem;
-      }
-    })
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1
-      })
-    }
-}
 
 function updateCartQuantity() {
   let cartQuantity=0;
