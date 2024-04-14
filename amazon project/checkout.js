3d1dd5d5992bb69f16fd8f10434d89ca3eb2b495
Mariama Seidu <153280802+Marie-297@users.noreply.github.com>
2024-04-14 13:14:29 +0100

    created an updateDelivery function to update the page each time we choose a different delivery option

--- amazon project/checkout.js
+++ amazon project/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from "./data/cart.js";
+import {cart, removeFromCart, updateDeliveryOption} from "./data/cart.js";
 import {products} from "./data/products.js";
 import { moneyFormat } from "./data/utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -82,7 +82,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
      const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
     html += 
       `
-        <div class="delivery-option">
+        <div class="delivery-option js-delivery-option" data-product-id=${matchingProduct.id} data-delivery-option-id=${deliveryOption.id}>
           <input type="radio" ${isChecked ? "checked" : ""}
             class="delivery-option-input"
             name="delivery-option-${matchingProduct.id}">
@@ -109,6 +109,13 @@ document.querySelectorAll(".js-delete-quantity-link").forEach((link) => {
     const container = document.querySelector(`.js-cart-item-container-${productId}`)
     container.remove();
   });
+})
+
+document.querySelectorAll(".js-delivery-option"). forEach((element) => {
+  element.addEventListener("click", () => {
+    const {productId, deliveryOptionId} = element.dataset;
+    updateDeliveryOption(deliveryOptionId, productId);
+  })
 })
 
 
