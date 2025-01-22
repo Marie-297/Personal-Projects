9a12d1dae7bb40179a055dd6a151ef0014122ef2
Mariama Seidu <153280802+Marie-297@users.noreply.github.com>
2025-01-22 20:44:23 +0000

    done

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Uppersection from './Components/Uppersection'
 import Lowersection from './Components/Lowersection'
+import img12 from './assets/images/image-12.png'
+import imgWedding from './assets/images/wedding-photography 1.png'
+import imgMountain from './assets/images/mountain-bike 1.png'
 
 function App() {
   return (
@@ -9,9 +12,9 @@ function App() {
       <Navbar />
       < Uppersection />
       <div className="flex-cards">
-        <Lowersection srcname='./../src/assets/images/image-12.png' rate='5.0' count='6' details='Life lessons with Katie Zaferes' details2='136'/>
-        <Lowersection srcname='./../src/assets/images/wedding-photography 1.png' rate='5.0' count='30' details='Learn wedding photography' details2='125'/>
-        <Lowersection srcname='./../src/assets/images/mountain-bike 1.png' rate='4.8' count='2' details='Group Mountain Biking' details2='50'/>
+        <Lowersection srcname={img12} count='6' details='Life lessons with Katie Zaferes' details2='136'/>
+        <Lowersection srcname={imgWedding} rate='5.0' count='30' details='Learn wedding photography' details2='125'/>
+        <Lowersection srcname={imgMountain} rate='4.8' count='2' details='Group Mountain Biking' details2='50'/>
       </div>
     </div>
   )
